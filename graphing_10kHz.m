9a687e137bbe26aaa961f11e3d951a5870254b51
Huzaifah Tariq Ahmed <huzihunter2782@yahoo.com>
2024-05-29 18:15:38 +0500

    Create graphing_10kHz.m

---
+++ graphing_10kHz.m
@@ -0,0 +1,28 @@
+% Open the file for reading
+fileID = fopen('10kHz.txt', 'r');
+
+% Read the data using fscanf
+data = fscanf(fileID, '# KERNEL: Time= %f, Counter= %d, DDS_Sin= %d\n', [3, Inf]);
+
+% Close the file
+fclose(fileID);
+
+% Extract counter, DDS_Sin, and time values
+time = data(1, :);
+counter = data(2, :);
+dds_sin = data(3, :);
+
+% Plot the graphs
+plot(time, dds_sin, "r", "LineWidth", 2);
+hold on
+stairs(time, dds_sin, "b", "LineWidth", 2);
+hold off
+
+% Add labels and title
+legend('Analog', 'Digital');
+xlabel('Time');
+ylabel('DDS Sin');
+title('10 kHZ Sine Wave Generated Using DDS');
+
+% Show the grid
+grid on;
